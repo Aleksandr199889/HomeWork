365750a1b3fd7a68368473f17144412cb7b63579
Aleksandr <aleks.remiks@mail.ru>
2022-12-08 23:42:41 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N % 23 == 0)
+{
+    if(N % 7 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Нет");
+}
