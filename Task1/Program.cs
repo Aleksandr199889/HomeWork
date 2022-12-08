435cad0ba2d93bda3fa1a99b159f1258eaa5f87d
Aleksandr <aleks.remiks@mail.ru>
2022-12-08 23:41:18 +0300

    task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите трёхзначное число");
 int N = Convert.ToInt32(Console.ReadLine());
-int F = N /10;
+int F = N / 10;
 int S = F % 10;
-Console.WriteLine("Вторая цифра" + F);
+Console.WriteLine("Вторая цифра" + S);
