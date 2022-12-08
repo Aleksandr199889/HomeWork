172de0a27905ee581588d9e9598e9b57e63513c4
Aleksandr <aleks.remiks@mail.ru>
2022-12-08 23:41:56 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while(N >= 1000)
+    {
+        N = N / 10;
+    }
+Console.WriteLine(N % 10);
+}
